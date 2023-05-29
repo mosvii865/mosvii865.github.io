1908bdf05a62549669f51c70942462f418570ca7
mosvii865 <73614541+mosvii865@users.noreply.github.com>
2023-05-29 13:16:04 -0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function showPopup() {
+    alert("¡Puedes encontrarnos en el Fraccionamiento Jardin de las moras Etapa 2. Calle Jardin de manzanos 1108!");
+  }
+  
+  function showPopup2() {
+    alert("¡Puedes contactarnos via WhatsApp al siguiente numero de telefono: 656-176-6201!");
+  }
+  
